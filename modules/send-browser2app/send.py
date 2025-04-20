cf896ddd96732252856b9bf7122bb15f37a8b959
ggiacomini2012 <ggiacomini2012@gmail.com>
2025-04-20 13:50:45 -0300

    update

--- modules/send-browser2app/send.py
+++ modules/send-browser2app/send.py
@@ -1,5 +1,7 @@
 import webbrowser
 import platform
+import time  # Import the time module
+import pyautogui
 
 number = "5547997676797"
 
@@ -48,8 +50,40 @@ else:
 # Open the URL if a browser was found
 if browser:
     try:
+            
         browser.open(url)
         print(f"Attempting to open {url}...")
+        if os_name == 'Windows':
+            time.sleep(2)
+            #navegate to chrome maybe with tab alt + tab
+            pyautogui.hotkey('alt', 'tab')
+            time.sleep(1)
+            #close one tab with pyautogui ctrl + w
+            #close one tab with pyautogui ctrl + w
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(1)
+            #go back to whatsapp app with pyautogui alt + tab
+            pyautogui.hotkey('alt', 'tab')
+            
+        if os_name == 'Darwin' or os_name == 'Linux':
+            time.sleep(1)
+            #navegate to chrome
+            pyautogui.hotkey('command', 'space')
+            time.sleep(1)
+            #type chrome
+            pyautogui.typewrite('chrome')
+            time.sleep(1)
+            #press enter
+            pyautogui.press('enter')
+            time.sleep(1)   
+            #close one tab with pyautogui ctrl + w
+            pyautogui.hotkey('ctrl', 'w')
+            time.sleep(1)
+            #go back to whatsapp app with pyautogui command + tab
+            pyautogui.hotkey('command', 'tab')
+            time.sleep(1)
+
+
     except Exception as e:
         print(f"Error opening URL: {e}")
 else:
@@ -60,3 +94,5 @@ else:
     #     webbrowser.open(url)
     # except Exception as e:
     #     print(f"Failed to open with system default: {e}")
+
+print(f"Running on OS: {os_name}")
